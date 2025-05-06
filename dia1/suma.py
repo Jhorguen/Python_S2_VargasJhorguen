0da278fb925cbab10106ca26d81ce3f5b4b5cb4f
Jhorguen <jhorguen123@gmail.com>
2025-05-05 21:30:23 -0500

    feat: :sparkles: New exercise Pseint to Python

---
+++ dia1/suma.py
@@ -0,0 +1,14 @@
+
+print("Hola bienvenido a mi calculadora")
+
+#PIDO Y CONVIERTO EN ENTERO CADA DATO INGRESADO#
+num1 = int(input("Ingresa el primer numero a sumar"))
+num2 = int(input("Ingresa el segundo numero a sumar"))
+
+
+#SUMO LOS DATOS INGRESADO Y CREO UNA VARIABLE PARA ALLI GUARDAR EL REULTADO DE DICHA SUMA#
+resultado = num1 + num2
+print(resultado)
+
+print("El resultado de tu suma es: "+ str(resultado))
+
