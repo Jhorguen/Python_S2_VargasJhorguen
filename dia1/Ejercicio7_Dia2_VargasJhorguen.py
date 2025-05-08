ca4df81b1ecf73fcd28f77d5bdd3548a11d416c4
jhorguen <you@example.comjhorguen123@gmail.com>
2025-05-08 10:14:58 -0500

    changes: :hammer: changes in organization of directories

---
+++ dia1/Ejercicio7_Dia2_VargasJhorguen.py
@@ -0,0 +1,12 @@
+# ##########################
+# #### Clase Dia 2 ######
+# ##########################
+
+#TABLA MULTIPLICAR - EJERCICIO 9#
+
+print("Dame el numero del cual deseas tener su tabla de multiplicar")
+num = int(input())
+
+for i in range(1, 11):
+    print(num*i)
+#Desarrollado por Jhorguen Vargas C.C 1.096.700.556#5
---
+++ dia1/main.py
@@ -0,0 +1,20 @@
+#CLASE DIA 1#
+print("Hola mundo")
+
+#CREACION DE VARIABLES#
+#1.DATO TIPO STRING#
+
+nombre = "Jhorguen"
+print(nombre)
+print(type(nombre))
+
+#2. DATO TIPO ENTERO#
+numeroEntero = 5
+print(numeroEntero)
+print(type(numeroEntero))
+
+#3. DATO TIPO NUMERO REAL#
+numeroReal = 5.3
+print(numeroReal)
+print(type(numeroReal))
+##Desarrollado por Jhorguen Vargas Moreno - CC: 1096700556##
