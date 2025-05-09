f7686d65dac7979b4baaa36ef003c53d547a8b28
Jhorguen <jhorguen123@gmail.com>
2025-05-09 09:33:24 -0500

    New changes
    New changes

--- dia1/suma.py
+++ dia1/suma.py
@@ -4,11 +4,9 @@ print("Hola bienvenido a mi calculadora")
 num1 = int(input("Ingresa el primer numero a sumar"))
 num2 = int(input("Ingresa el segundo numero a sumar"))
 
-
 #SUMO LOS DATOS INGRESADO Y CREO UNA VARIABLE PARA ALLI GUARDAR EL REULTADO DE DICHA SUMA#
 resultado = num1 + num2
 print(resultado)
-
 
 #IMPRIMO LA SUMNA DE LOS DOS NUMERO CONCATENANDO LA VARIABLE RESULTADO CONVIRTIENDOLA EN STRING PARA PORDER CONCATENARLA#
 print("El resultado de tu suma es: "+ str(resultado))
